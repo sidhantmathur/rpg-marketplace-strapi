80f9aca316cbcb3ae5075c0521fa686208d9c5ef
sidhant <sidhant185@gmail.com>
2025-04-16 21:54:20 -0400

    fix nextrequest

--- src/app/api/profile/[id]/route.ts
+++ src/app/api/profile/[id]/route.ts
@@ -1,10 +1,14 @@
 import { PrismaClient } from '@prisma/client';
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
 
 const prisma = new PrismaClient();
 
-export async function GET(req: NextRequest, context: { params: { id: string } }) {
-  const id = context.params.id;
+export async function GET(
+  _req: NextRequest,
+  context: { params: { id: string } }
+) {
+  const { id } = context.params;
 
   const profile = await prisma.profile.findUnique({
     where: { id },
