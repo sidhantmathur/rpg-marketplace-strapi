d6e5a7768f444d5031e9f16baa7d2e8828e39836
sidhant <sidhant185@gmail.com>
2025-04-17 00:00:46 -0400

    attempt to fix params.id

--- src/app/api/profile/[id]/route.ts
+++ src/app/api/profile/[id]/route.ts
@@ -1,10 +1,15 @@
 import { PrismaClient } from '@prisma/client';
 import { NextRequest, NextResponse } from 'next/server';
 
+export const dynamic = 'force-dynamic';
+
 const prisma = new PrismaClient();
 
-export async function GET(req: NextRequest, context: { params: { id: string } }) {
-  const { id } = context.params;
+export async function GET(
+  req: NextRequest,
+  context: { params: { id: string } }
+) {
+  const id = context.params.id; 
 
   const profile = await prisma.profile.findUnique({
     where: { id },
--- src/app/api/user-joined-sessions/[id]/route.ts
+++ src/app/api/user-joined-sessions/[id]/route.ts
@@ -1,10 +1,14 @@
 import { PrismaClient } from '@prisma/client';
 import { NextRequest, NextResponse } from 'next/server';
 
+export const dynamic = 'force-dynamic';
 const prisma = new PrismaClient();
 
-export async function GET(req: NextRequest, context: { params: { id: string } }) {
-  const { id } = context.params;
+export async function GET(
+  req: NextRequest,
+  context: { params: { id: string } }
+) {
+  const id = context.params.id;
 
   const bookings = await prisma.booking.findMany({
     where: { userId: id },
