069184b2db0e40b0d96c87a35f51d34e92427d86
sidhant <sidhant185@gmail.com>
2025-04-16 23:01:34 -0400

    context any like we did for profile id

--- src/app/api/user-joined-sessions/[id]/route.ts
+++ src/app/api/user-joined-sessions/[id]/route.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from 'next/server';
 
 const prisma = new PrismaClient();
 
-export async function GET(_: NextRequest, context: { params: { id: string } }) {
+export async function GET(_: NextRequest, context: any) {
   const bookings = await prisma.booking.findMany({
     where: { userId: context.params.id },
     select: { sessionId: true },
