b870df6c18ebbf12b36a81c82624d253c0167c37
sidhant <sidhant185@gmail.com>
2025-05-07 09:42:15 -0400

    fixed prisma import statement to get build working again

--- src/app/api/sessions/route.ts
+++ src/app/api/sessions/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { prisma } from "@/lib/prisma";
+import prisma from "@/lib/prisma";
 import { supabase } from "@/lib/supabaseClient";
 
 export async function GET(request: Request) {
