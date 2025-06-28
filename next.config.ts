15b22cf852789d9ffa00a2eff8bb296f849bb2d1
sidhant <sidhant185@gmail.com>
2025-06-27 21:05:17 -0400

    vercel changes

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: false,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**.supabase.co",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "cdn.arstechnica.net",
-        pathname: "/**",
-      },
-    ],
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
